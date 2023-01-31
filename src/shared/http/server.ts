c766236500c0b14865f037656de8b207d05ddf69
Matheus Paulo Silva <mah.t19998@gmail.com>
2023-01-31 16:15:31 -0300

    Add lib dotenv

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,14 +1,16 @@
+import 'dotenv/config'
 import express from 'express'
 import 'express-async-errors'
+import cors from 'cors'
 
 const app = express()
-
+app.use(cors)
 app.use(express.json())
 
 app.get('/', (request, response) => {
   return response.json({ message: 'Olá Dev!' })
 })
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000!')
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT}!`)
 })
