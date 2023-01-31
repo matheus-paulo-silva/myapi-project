fb13cb0098d41811b091c979966799cfee32ba64
Matheus Paulo Silva <mah.t19998@gmail.com>
2023-01-31 17:39:15 -0300

    Adjustment localhost dont open

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,14 +2,15 @@ import 'dotenv/config'
 import express from 'express'
 import 'express-async-errors'
 import cors from 'cors'
-import { routes } from './routes'
 
 const app = express()
-app.use(cors)
+app.use(cors())
 app.use(express.json())
 
-app.use(routes)
+app.get('/', (request, response) => {
+  return response.json({ message: 'Olá Dev!' })
+})
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server started on port ${process.env.PORT}!`)
+  console.log(`Server started on port ${process.env.PORT}! 🏆`)
 })
