fe02a1acfe5d72881932698e984f9fb8156b4603
Matheus Paulo Silva <mah.t19998@gmail.com>
2023-02-08 18:28:28 -0300

    Create migration CreateRolesTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,8 +1,9 @@
 import { DataSource } from 'typeorm'
+import { CreateRolesTable1675890785680 } from './migrations/1675890785680-CreateRolesTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [],
-  migrations: [],
+  migrations: [CreateRolesTable1675890785680],
 })
