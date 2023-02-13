70985e47faf2bff47b2af59147c01e690865dbda
Matheus Paulo Silva <mah.t19998@gmail.com>
2023-02-13 19:21:44 -0300

    Add container and refactor useCases

--- src/roles/useCases/deleteRole/DeleteRoleUseCase.ts
+++ src/roles/useCases/deleteRole/DeleteRoleUseCase.ts
@@ -1,13 +1,17 @@
-import { Role } from '@roles/entities/Role'
-import { RolesRepository } from '@roles/repositories/RolesRepository'
+import { IRolesRepository } from '@roles/repositories/IRolesRepository'
 import { AppError } from '@shared/errors/AppError'
+import { injectable, inject } from 'tsyringe'
 
 type DeleteRoleParams = {
   id: string
 }
 
+@injectable()
 export class DeleteRoleUseCase {
-  constructor(private rolesRepository: RolesRepository) {}
+  constructor(
+    @inject('RoleRepository')
+    private rolesRepository: IRolesRepository,
+  ) {}
 
   async execute({ id }: DeleteRoleParams): Promise<void> {
     const role = await this.rolesRepository.findById(id)
