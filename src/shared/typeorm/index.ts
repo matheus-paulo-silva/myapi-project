bc8a2c5af464c9c8869979b48413bb7c8088b4bf
Matheus Paulo Silva <mah.t19998@gmail.com>
2023-02-14 19:39:42 -0300

    Create migration AddRoleIdToUsersTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -2,10 +2,15 @@ import { Role } from '@roles/entities/Role'
 import { DataSource } from 'typeorm'
 import { CreateRolesTable1675890785680 } from './migrations/1675890785680-CreateRolesTable'
 import { CreateUsersTable1676413226278 } from './migrations/1676413226278-CreateUsersTable'
+import { AddRoleIdToUsersTable1676413740431 } from './migrations/1676413740431-AddRoleIdToUsersTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [Role],
-  migrations: [CreateRolesTable1675890785680, CreateUsersTable1676413226278],
+  migrations: [
+    CreateRolesTable1675890785680,
+    CreateUsersTable1676413226278,
+    AddRoleIdToUsersTable1676413740431,
+  ],
 })
---
+++ src/shared/typeorm/migrations/1676413740431-AddRoleIdToUsersTable.ts
@@ -0,0 +1,36 @@
+import { query } from 'express'
+
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm'
+
+export class AddRoleIdToUsersTable1676413740431 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'users',
+      new TableColumn({
+        name: 'roleId',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    )
+    await queryRunner.createForeignKey(
+      'users',
+      new TableForeignKey({
+        name: 'UsersRoles',
+        columnNames: ['roleId'],
+        referencedTableName: 'roles',
+        referencedColumnNames: ['id'],
+        onDelete: 'SET NULL',
+      }),
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('users', 'UsersRoles')
+    await queryRunner.dropColumn('users', 'roleId')
+  }
+}
