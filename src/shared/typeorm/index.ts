adc17f4e7eae83c87fbae46704afd9d454f4a4e1
Matheus Paulo Silva <mah.t19998@gmail.com>
2023-02-14 19:43:16 -0300

    Create user entitiy

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,4 +1,5 @@
 import { Role } from '@roles/entities/Role'
+import { User } from '@users/entities/User'
 import { DataSource } from 'typeorm'
 import { CreateRolesTable1675890785680 } from './migrations/1675890785680-CreateRolesTable'
 import { CreateUsersTable1676413226278 } from './migrations/1676413226278-CreateUsersTable'
@@ -7,7 +8,7 @@ import { AddRoleIdToUsersTable1676413740431 } from './migrations/1676413740431-A
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [Role],
+  entities: [Role, User],
   migrations: [
     CreateRolesTable1675890785680,
     CreateUsersTable1676413226278,
