836d542062b2ce99a88cc9b3f333c22be44a5bc3
Matheus Paulo Silva <mah.t19998@gmail.com>
2023-03-08 20:23:41 -0300

    Create RefreshToken - interface

--- src/users/repositories/IUsersRepository.ts
+++ src/users/repositories/IUsersRepository.ts
@@ -1,37 +1,19 @@
-import { Role } from '@roles/entities/Role'
-import { User } from '@users/entities/User'
+import { RefreshToken } from '@users/entities/RefreshToken'
 
-export type CreateUserDTO = {
-  name: string
-  email: string
-  password: string
-  isAdmin: boolean
-  role: Role
+export type CreateRefreshTokenDTO = {
+  user_id: string
+  token: string
+  valid: boolean
+  expires: Date
 }
 
-export type PaginateParams = {
-  page: number
-  skip: number
-  take: number
-}
-
-export type UsersPaginateProperties = {
-  per_page: number
-  total: number
-  current_page: number
-  data: User[]
-}
-
-export interface IUsersRepository {
-  create({ name, email, password, isAdmin, role }: CreateUserDTO): Promise<User>
-  save(user: User): Promise<User>
-  findAll({
-    page,
-    skip,
-    take,
-  }: PaginateParams): Promise<UsersPaginateProperties>
-  findById(id: string): Promise<User | null>
-  findByName(name: string): Promise<User | null>
-  findByEmail(email: string): Promise<User | null>
-  delete(user: User): Promise<void>
+export interface IRefreshTokenRepository {
+  create({
+    user_id,
+    token,
+    valid,
+    expires,
+  }: CreateRefreshTokenDTO): Promise<RefreshToken>
+  findByToken(token: string): Promise<RefreshToken | null>
+  invalidate(refresh_token: RefreshToken): Promise<void>
 }
